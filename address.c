7a67abce66bc51e0327290ba3ed40d8ad8ec146d
Akiicodes <akshit.sinha1356@gmail.com>
2025-09-08 14:54:05 +0530

    Add files via upload
    exp 1 q2

---
+++ address.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Printing address in multiple lines
+    printf("Akshit Sinha\n");
+    printf("UPES Dehradun\n");
+    printf("Bidholi Campus\n");
+    printf("Dehradun, Uttarakhand\n");
+    printf("PIN - 248007\n");
+
+    return 0;
+}
